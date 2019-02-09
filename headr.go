7f25260910723062f3d5e9d64e29b1dbdd3cfb31
Klint Mane <klintmane@gmail.com>
2019-02-09 21:15:39 +0100

    Add missing spacing

--- headr.go
+++ headr.go
@@ -25,7 +25,7 @@ func New(h http.Handler) *Headr {
 
 // Set adds header entries with the given key to the HTTP handler decorator
 func (h *Headr) Set(key string, values []string) {
-	value := strings.Join(values, ",")
+	value := strings.Join(values, ", ")
 	header := []string{key, value}
 	h.headers = append(h.headers, header)
 }
